8c4211c8af5ce9fd506392de06f4528d3178e822
Ribiro <deno059861>
2023-03-31 00:09:36 +0300

    task 11

---
+++ task11.py
@@ -0,0 +1,9 @@
+# Write a program that takes the date of birth of a person and the program outputs the age in terms of years,months,days TODAY.
+import datetime
+
+date_of_birth = input("Enter your date of birth (YYYY-MM-DD): ")
+
+
+today = datetime.date.today()
+today = str(today)
+
